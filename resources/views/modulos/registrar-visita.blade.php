6a72fba2ff0b66fe18a638741d4ddc38886f3855
wdavid <wdavidcalsin968@gmail.com>
2023-04-19 23:19:00 -0500

    feat: add current date value for registration

--- resources/views/modulos/registrar-visita.blade.php
+++ resources/views/modulos/registrar-visita.blade.php
@@ -77,11 +77,10 @@
                     </div>
                     <div class="mb-8 flex flex-wrap -mx-3 mb-2">
                         <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-
                             <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="fecha_y_hora">
                                 FECHA Y HORA DE REGISTRO
                             </label>
-                            <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="fecha_y_hora" name="fecha_y_hora" type="datetime-local" placeholder="">
+                            <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="fecha_y_hora" name="fecha_y_hora" type="datetime-local" placeholder="" value="{{ date('Y-m-d\TH:i:s') }}">
                             @error('fecha_y_hora')
                                 <p class="text-red-500 text-xs italic">{{ $message }}</p>
                             @enderror
