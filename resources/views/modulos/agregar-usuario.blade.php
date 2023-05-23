2b45149b8c8f8e7eb4562e335494f3015f877e74
WdavidCalsin <wdavidcalsin968@gmail.com>
2023-05-22 22:10:15 -0500

    fix: update in add user with roleI

--- resources/views/modulos/agregar-usuario.blade.php
+++ resources/views/modulos/agregar-usuario.blade.php
@@ -12,33 +12,33 @@
                         <div class="flex-grow border-t border-gray-400"></div>
                     </div>
                     @if(session('message'))
-                        <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative" role="alert">
-                            <strong class="font-bold">¡Éxito!</strong>
-                            <span class="block sm:inline">{{ session('message') }}</span>
-                            <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
-                                <svg class="fill-current h-6 w-6 text-green-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                    <title>Close</title>
-                                    <path d="M14.348 5.652a.999.999 0 1 0-1.414-1.414L10 8.586 6.066 4.652a.999.999 0 1 0-1.414 1.414L8.586 10l-3.934 3.934a.999.999 0 1 0 1.414 1.414L10 11.414l3.934 3.934a.999.999 0 1 0 1.414-1.414L11.414 10l3.934-3.934z"/>
-                                </svg>
-                            </span>
-                        </div>
+                    <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative" role="alert">
+                        <strong class="font-bold">¡Éxito!</strong>
+                        <span class="block sm:inline">{{ session('message') }}</span>
+                        <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+                            <svg class="fill-current h-6 w-6 text-green-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                <title>Close</title>
+                                <path d="M14.348 5.652a.999.999 0 1 0-1.414-1.414L10 8.586 6.066 4.652a.999.999 0 1 0-1.414 1.414L8.586 10l-3.934 3.934a.999.999 0 1 0 1.414 1.414L10 11.414l3.934 3.934a.999.999 0 1 0 1.414-1.414L11.414 10l3.934-3.934z" />
+                            </svg>
+                        </span>
+                    </div>
                     @endif
 
                     @if(session('error'))
-                        <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
-                            <strong class="font-bold">¡Error!</strong>
-                            <span class="block sm:inline">{{ session('error') }}</span>
-                            <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
-                                <svg class="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                    <title>Close</title>
-                                    <path d="M14.348 5.652a.999.999 0 1 0-1.414-1.414L10 8.586 6.066 4.652a.999.999 0 1 0-1.414 1.414L8.586 10l-3.934 3.934a.999.999 0 1 0 1.414 1.414L10 11.414l3.934 3.934a.999.999 0 1 0 1.414-1.414L11.414 10l3.934-3.934z"/>
-                                </svg>
-                            </span>
-                        </div>
+                    <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+                        <strong class="font-bold">¡Error!</strong>
+                        <span class="block sm:inline">{{ session('error') }}</span>
+                        <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+                            <svg class="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                <title>Close</title>
+                                <path d="M14.348 5.652a.999.999 0 1 0-1.414-1.414L10 8.586 6.066 4.652a.999.999 0 1 0-1.414 1.414L8.586 10l-3.934 3.934a.999.999 0 1 0 1.414 1.414L10 11.414l3.934 3.934a.999.999 0 1 0 1.414-1.414L11.414 10l3.934-3.934z" />
+                            </svg>
+                        </span>
+                    </div>
                     @endif
 
                     <form method="POST" action="{{ route('agregar-usuario.store') }}">
-                        @csrf    
+                        @csrf
                         <div class="flex flex-col gap-6 ">
                             <div class="flex flex-wrap -mx-3 mb-2">
                                 <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
@@ -47,7 +47,7 @@
                                     </label>
                                     <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" placeholder="Ingrese nombres y apellidos completo" id="nombre" type="text" name="nombre" value="" required>
                                     @error('nombre')
-                                        <p class="text-red-500 text-xs italic">{{ $message }}</p>
+                                    <p class="text-red-500 text-xs italic">{{ $message }}</p>
                                     @enderror
                                 </div>
                                 <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
@@ -55,25 +55,25 @@
                                         DNI:
                                     </label>
                                     <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" placeholder="Ingrese su DNI" id="dni" type="number" name="dni" value="" required>
-                                     @error('dni')
-                                        <p class="text-red-500 text-xs italic">{{ $message }}</p>
+                                    @error('dni')
+                                    <p class="text-red-500 text-xs italic">{{ $message }}</p>
                                     @enderror
                                 </div>
                             </div>
                             <div class="flex flex-wrap -mx-3 mb-2">
                                 <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
                                     <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="rol">
-                                    TIPO DE USUARIO:
+                                        TIPO DE USUARIO:
                                     </label>
                                     <div class="relative">
                                         <select class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="rol" name="rol" required>
                                             <option value="" disabled selected>Seleccione...</option>
-                                            <option default value="personero">Personero</option>    
+                                            <option default value="guardiania">Guardiania</option>
                                             <option value="admin">Admin</option>
-                                            <option value="supervisor">Supervisor</option>     
+                                            <option value="supervisor">Supervisor</option>
                                         </select>
                                         @error('rol')
-                                            <p class="text-red-500 text-xs italic">{{ $message }}</p>
+                                        <p class="text-red-500 text-xs italic">{{ $message }}</p>
                                         @enderror
 
                                     </div>
@@ -84,11 +84,11 @@
                                     </label>
                                     <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" placeholder="example@example.com" id="correo" name="correo" type="email" value="" required>
                                     @error('correo')
-                                        <p class="text-red-500 text-xs italic">{{ $message }}</p>
+                                    <p class="text-red-500 text-xs italic">{{ $message }}</p>
                                     @enderror
                                 </div>
                             </div>
-                            
+
                             <div class="flex flex-wrap -mx-3 mb-2">
                                 <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
                                     <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="contrasena">
@@ -96,7 +96,7 @@
                                     </label>
                                     <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" placeholder="••••••••••••••••" id="contrasena" type="password" name="contrasena" value="" required>
                                     @error('contrasena')
-                                        <p class="text-red-500 text-xs italic">{{ $message }}</p>
+                                    <p class="text-red-500 text-xs italic">{{ $message }}</p>
                                     @enderror
                                 </div>
                                 <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
@@ -106,7 +106,7 @@
                                     <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" placeholder="••••••••••••••••" id="confirm_contrasena" type="password" name="confirm_contrasena" value="" required oninput="check(this)">
                                     <span id="message"></span>
                                     @error('confirm_contrasena')
-                                        <p class="text-red-500 text-xs italic">{{ $message }}</p>
+                                    <p class="text-red-500 text-xs italic">{{ $message }}</p>
                                     @enderror
                                 </div>
                             </div>
@@ -117,8 +117,8 @@
                                     </label>
                                     <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" placeholder="999-999-999" id="telefono" type="text" name="telefono" value="" required>
                                     @error('telefono')
-                                        <p class="text-red-500 text-xs italic">{{ $message }}</p>
-                                    @enderror 
+                                    <p class="text-red-500 text-xs italic">{{ $message }}</p>
+                                    @enderror
                                 </div>
                             </div>
                             <script>
@@ -136,32 +136,31 @@
                                 </button>
                             </div>
                         </div>
-                    </form>    
+                    </form>
                 </div>
             </div>
         </div>
     </nav>
     @if(session('message'))
-        <script>
-            Swal.fire({
-                position: 'top-center',
-                icon: 'success',
-                title: "{{session('message')}}",
-                showConfirmButton: false,
-                timer: 1500
-            })
-        </script>
+    <script>
+        Swal.fire({
+            position: 'top-center',
+            icon: 'success',
+            title: "{{session('message')}}",
+            showConfirmButton: false,
+            timer: 1500
+        })
+    </script>
     @endif
     @if(session('error'))
-        <script>
-            Swal.fire({
-                position: 'top-center',
-                icon: 'error',
-                title: "{{session('error')}}",
-                showConfirmButton: false,
-                timer: 1500
-            })
-        </script>
+    <script>
+        Swal.fire({
+            position: 'top-center',
+            icon: 'error',
+            title: "{{session('error')}}",
+            showConfirmButton: false,
+            timer: 1500
+        })
+    </script>
     @endif
 </x-app-layout>
-
